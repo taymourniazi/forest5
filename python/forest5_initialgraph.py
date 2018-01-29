03e2a657bc670e5dbbd0231134d39061faed6066
Taymour Khan Niazi <30389323+taymourniazi@users.noreply.github.com>
2018-01-29 13:56:06 +0500

    Initial graphs code

---
+++ python/forest5_initialgraph.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 29 12:19:10 2018
+
+@author: asd
+"""
+#Importing Library
+import scipy.stats
+import numpy as np
+import statistics
+import pandas as pd
+import matplotlib as plt
+#Importing dataset
+dataset = pd.read_csv('forest5.csv')
+meanloc =dataset.iloc[:, 1].values
+#MEAN with numpy
+meanelevation=np.mean(meanloc)
+print(meanelevation)
+#MEAN with statistics
+meanselevation=statistics.mean(meanloc)
+print(meanselevation)
+#MEDIAN from numpy
+medianelevation=np.median(meanloc)
+print(medianelevation)
+#MEDIAN from statistics
+medianselevation=statistics.median(meanloc)
+print(medianselevation)
+#MODE with scipy
+modeelevation=scipy.stats.mode(meanloc)
+print(modeelevation)
+#MODE with numpy
+modeselevation=statistics.mode(meanloc)
+print(modeselevation)
+#best way to get statistics detail of individual variable
+from scipy import stats
+print(stats.describe(meanloc))
+
+# creaing plots
+import seaborn as sns
+sns.set()
+import matplotlib.pyplot as plpt
+plpt.hist(meanloc)
+plt.show()
